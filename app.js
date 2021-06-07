254022e3d345cc8d721922c3522fa27d47cf5c1d
AL-HUSSEIN ABUBAKAR <al-hussein.abubakar@amalitech.org>
2021-06-07 03:17:46 +0000

    modified

--- app.js
+++ app.js
@@ -5,32 +5,32 @@ first.addEventListener('click', () => {
     let bmi = Math.round(weight / (height ** 2));
     let result = document.getElementById("result");
     let message = "";
-    let color = "";
+    let color = "";   
 
     if (bmi < 18.5) {
-        message = ", you are underweight";
+        message = ", so you are underweight";
         color = "#FFD016";
+        img='images/underweight.png';
 
     } else if ((bmi >= 18.5) && (bmi <= 24.9)) {
-        message = ", you have normal weight";
+        message = ", so you have normal weight";
         color = "#538313";
 
 
-    } else if ((bmi > 24.9) && (bmi >= 29.9)) {
-        message = ", you are overweight";
+    } else if ((bmi > 24.9) && (bmi <= 29.9)) {
+        message = ",so  you are overweight";
         color = "#fd802e";
 
 
     } else if ((bmi > 29.9) && (bmi <= 35)) {
-        message = " , you are obese";
+        message = " ,so you are obese";
         color = "#F26450";
 
     } else {
-        message = " , you are extremely obese";
+        message = " , so you are extremely obese";
         color = "#CB2028";
 
     }
     result.innerHTML = "Your BMI is " + bmi + message;
-    result.style.color = color;
-
+    result.style.color = color;    
 }, false);
